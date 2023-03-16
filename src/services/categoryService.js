8ebc797b969d7fecfc62bc3b41f75e054ed75068
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-17 00:43:43 +0100

    fix: refactor code

--- src/services/categoryService.js
+++ src/services/categoryService.js
@@ -2,10 +2,6 @@ import http from './httpService';
 
 const apiEndPoint = '/categories';
 
-export function getCategories() {
-  return http.get(apiEndPoint);
-};
+export const getCategories = () => http.get(apiEndPoint);
 
-export function createCategory(category) {
-  return http.post(apiEndPoint, category);
-};
+export const createCategory = (category) => http.post(apiEndPoint, category);

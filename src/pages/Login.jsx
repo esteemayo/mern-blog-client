98e6303c2185f848d64f49b8135f386911b6698a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-17 00:21:40 +0100

    fix: modify

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useRef } from 'react';
 import { Link } from 'react-router-dom';
 
+import Button from 'components/Button';
 import { useGlobalContext } from 'context/GlobalState';
 import { loginUser } from 'services/userService';
 import { LOGIN_START } from 'context/Types';

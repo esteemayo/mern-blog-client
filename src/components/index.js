65c51617a97b329e8b1052cde90e6e2df93a0b1f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-16 03:15:02 +0100

    fix: modify

--- src/components/index.js
+++ src/components/index.js
@@ -9,8 +9,6 @@ import Posts from './Posts';
 import SideBar from './Sidebar';
 import SinglePost from './SinglePost';
 import Spinner from './Spinner';
-import Button from './Button';
-import Header from './Header';
 import TopBar from './TopBar';
 import UserData from './UserData';
 import UserPassword from './UserPassword';

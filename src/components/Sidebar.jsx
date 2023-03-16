515546c511d25238bc8a0914897387c8bfacae9e
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-17 00:08:55 +0100

    fix: modify

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,5 @@
 import { Link } from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 import {
   FaFacebookSquare,
   FaTwitterSquare,

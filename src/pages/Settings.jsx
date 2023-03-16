10a01a9304d51fe44462ce3992ba52ccbdfba0a6
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-16 03:07:52 +0100

    fix: change file extension to jsx

---
+++ src/pages/Settings.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import { deleteCurrentUser } from '../services/userService';
+import { useGlobalContext } from '../context/GlobalState';
+import { Account, SideBar } from '../components'
+
+const Settings = () => {
+  const { user, logout } = useGlobalContext();
+
+  const deleteMe = async () => {
+    try {
+      await deleteCurrentUser();
+      logout();
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div className='settings'>
+      <div className='settingsWrapper'>
+        <div className='settingsTitle'>
+          <span className='settingsUpdateTitle'>Update Your Account</span>
+          {user.role !== 'admin' && (
+            <span className='settingsDeleteTitle' onClick={deleteMe}>
+              Delete Account
+            </span>
+          )}
+        </div>
+        <Account />
+      </div>
+      <SideBar />
+    </div>
+  );
+};
+
+export default Settings;

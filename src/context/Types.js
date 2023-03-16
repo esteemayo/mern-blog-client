83d13cfbbf656e20b5e835f1e867059d977c58ae
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-17 00:51:54 +0100

    fix: rename file

--- src/context/Types.js
+++
@@ -1,7 +0,0 @@
-export const LOGOUT = 'LOGOUT';
-export const LOGIN_START = 'LOGIN_START';
-export const UPDATE_START = 'UPDATE_START';
-export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
-export const LOGIN_FAILURE = 'LOGIN_FAILURE';
-export const UPDATE_FAILURE = 'UPDATE_FAILURE';
-export const UPDATE_SUCCESS = 'UPDATE_SUCCESS';

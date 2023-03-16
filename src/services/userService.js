53db1acbd1505680b88b1890e9e35be4fafec0d2
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-17 00:40:45 +0100

    fix: refactor code

--- src/services/userService.js
+++ src/services/userService.js
@@ -5,25 +5,15 @@ const apiEndPoint = '/users';
 
 http.setJwt(getJwt());
 
-export function registerUser(userData) {
-  return http.post(`${apiEndPoint}/signup`, userData);
-};
+export const registerUser = (userData) => http.post(`${apiEndPoint}/signup`, userData);
 
-export function loginUser(userData) {
-  return http.post(`${apiEndPoint}/login`, userData);
-};
+export const loginUser = (userData) => http.post(`${apiEndPoint}/login`, userData);
 
-export function updateUserData(userData) {
-  return http.patch(`${apiEndPoint}/update-me`, userData);
-};
+export const updateUserData = (userData) => http.patch(`${apiEndPoint}/update-me`, userData);
 
-export function updateUserPassword(userData) {
-  return http.patch(`${apiEndPoint}/update-my-password`, userData);
-};
+export const updateUserPassword = (userData) => http.patch(`${apiEndPoint}/update-my-password`, userData);
 
-export function deleteCurrentUser() {
-  return http.delete(`${apiEndPoint}/delete-me`);
-};
+export const deleteCurrentUser = () => http.delete(`${apiEndPoint}/delete-me`);
 
 function getJwt() {
   return localStorage.getItem(tokenKey);

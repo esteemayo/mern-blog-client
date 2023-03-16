ea6f1de7a136af9ecd763fbd11dd35de3f9ec909
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-17 00:42:15 +0100

    fix: format code

--- src/services/userService.js
+++ src/services/userService.js
@@ -7,7 +7,8 @@ http.setJwt(getJwt());
 
 export const registerUser = (userData) => http.post(`${apiEndPoint}/signup`, userData);
 
-export const loginUser = (userData) => http.post(`${apiEndPoint}/login`, userData);
+export const loginUser = (userData) =>
+  http.post(`${apiEndPoint}/login`, userData);
 
 export const updateUserData = (userData) =>
   http.patch(`${apiEndPoint}/update-me`, userData);

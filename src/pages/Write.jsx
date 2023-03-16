841ae992119a18c25272f33a42a9aa63ae854bf1
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-17 00:16:46 +0100

    fix: modify

--- src/pages/Write.jsx
+++ src/pages/Write.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { FaPlus } from 'react-icons/fa';
 
+import Button from 'components/Button';
 import { getCategories } from '../services/categoryService';
 import { createPost } from '../services/postService';
 import { upload } from '../services/uploadService';

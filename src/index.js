0ab317aa3ff9138380b4affb94cb4104f51162df
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-17 00:52:47 +0100

    fix: file directory

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { AppProvider } from './context/GlobalState';
+import { AppProvider } from './context/auth/AuthContext';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(

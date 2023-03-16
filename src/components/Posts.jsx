6b4e8abb7b7eb9b1d71d1b6dec554b3de327bd94
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-16 03:09:53 +0100

    fix: change file extension to jsx

---
+++ src/components/Posts.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Post } from './index';
+
+const Posts = ({ posts }) => {
+  return (
+    <div className='posts'>
+      {posts.map((post) => {
+        return <Post key={post._id} {...post} />;
+      })}
+    </div>
+  );
+};
+
+export default Posts;

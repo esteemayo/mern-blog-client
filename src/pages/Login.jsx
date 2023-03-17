e8d1dc76740b0011c5c2d1bd9952bce4e27bba9e
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-17 01:45:13 +0100

    fix: modify

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -23,7 +23,7 @@ const Login = () => {
       };
 
       const { data } = await loginUser(userData);
-      loginSuccess(data);
+      loginSuccess(data.details);
     } catch (err) {
       loginFailure();
       console.log(err);

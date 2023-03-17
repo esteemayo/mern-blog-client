8f224fa669b49d7770ea9bd7bfe98382446f80c6
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-17 01:48:41 +0100

    feat: import removeFromStorage function

--- src/context/auth/AuthContext.js
+++ src/context/auth/AuthContext.js
@@ -1,6 +1,6 @@
 import jwtDecode from 'jwt-decode';
 import React, { useContext, useReducer, createContext } from 'react';
-import { getFromStorage, tokenKey } from 'utils';
+import { getFromStorage, removeFromStorage, tokenKey } from 'utils';
 
 import Reducer from './AuthReducer';
 import * as actions from './AuthTypes';

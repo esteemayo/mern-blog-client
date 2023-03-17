1739298c4de452fc4e26779d6e9850b1f364db82
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-17 01:52:15 +0100

    fix: modify

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,7 +5,6 @@ import Button from 'components/Button';
 import { useGlobalContext } from 'context/auth/AuthContext';
 import { loginUser } from 'services/userService';
 import { LOGIN_START } from 'context/auth/AuthTypes';
-import { loginUser } from 'services/userService';
 
 const Login = () => {
   const { isFetching, dispatch, loginSuccess, loginFailure } =

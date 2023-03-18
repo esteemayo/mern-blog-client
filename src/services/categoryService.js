b2e42823d7d084d9a04a4d11ca510f8ad6eeda5c
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-18 01:26:11 +0100

    feat: add clean up function

--- src/services/categoryService.js
+++ src/services/categoryService.js
@@ -2,7 +2,7 @@ import http from './httpService';
 
 const apiEndPoint = '/categories';
 
-export const getCategories = () => http.get(apiEndPoint);
+export const getCategories = (token) => http.get(apiEndPoint, { cancelToken: token });
 
 export const createCategory = (category) =>
   http.post(apiEndPoint, category);

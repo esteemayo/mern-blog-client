c901b66bb128f53fdd7ca57a98f0b6c85a9084d2
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-18 01:26:36 +0100

    fix: format code

--- src/services/categoryService.js
+++ src/services/categoryService.js
@@ -2,7 +2,8 @@ import http from './httpService';
 
 const apiEndPoint = '/categories';
 
-export const getCategories = (token) => http.get(apiEndPoint, { cancelToken: token });
+export const getCategories = (token) =>
+  http.get(apiEndPoint, { cancelToken: token });
 
 export const createCategory = (category) =>
   http.post(apiEndPoint, category);

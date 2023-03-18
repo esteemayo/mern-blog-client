c905ae967d1ba1aefac1f470e0c8cce6bc2c2be7
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-18 01:27:49 +0100

    feat: import axios module

--- src/pages/Write.jsx
+++ src/pages/Write.jsx
@@ -5,6 +5,7 @@ import Button from 'components/Button';
 import { getCategories } from 'services/categoryService';
 import { upload } from 'services/uploadService';
 import { createPost } from 'services/postService';
+import axios from 'axios';
 
 const Write = () => {
   const [file, setFile] = useState(null);

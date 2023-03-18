4f585cd0b5d75cc9cb3d649265fa089df8265041
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-18 01:20:45 +0100

    fix: modify

--- src/components/SinglePost.jsx
+++ src/components/SinglePost.jsx
@@ -8,7 +8,6 @@ import { Link } from 'react-router-dom';
 import Button from './Button';
 import { useGlobalContext } from 'context/auth/AuthContext';
 import * as postAPI from 'services/postAPI';
-import axios from 'axios';
 
 const PF = 'http://localhost:8080/images/';
 

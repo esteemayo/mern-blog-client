0e7fc1473cfb91cb9c3b0c19cf15f1f604b45bf3
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-29 00:46:50 +0100

    fix: fix a bug

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import AppProvider from 'context/auth/AuthContext';
+import AppProvider from './context/auth/AuthContext';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 

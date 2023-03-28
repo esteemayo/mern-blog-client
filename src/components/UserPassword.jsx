1460b12596e4f26c2308ae3c027513a9dfd8a6c5
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-29 00:50:02 +0100

    fix: fix an error

--- src/components/UserPassword.jsx
+++ src/components/UserPassword.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 import Button from './Button';
 import { updateUserPassword } from '../services/userService';
-import { UPDATE_START } from 'context/auth/AuthTypes';
+import { UPDATE_START } from '../context/auth/AuthTypes';
 import { useGlobalContext } from 'context/auth/AuthContext';
 
 const initialState = {

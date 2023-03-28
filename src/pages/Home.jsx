faa06faf78e41a131834d51831383a24372f16e0
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-29 00:44:18 +0100

    fix: fix a bug

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import { useLocation } from 'react-router-dom';
 import Posts from '../components/Posts';
 import SideBar from '../components/Sidebar';
 import Header from '../components/Header';
-import { getPosts } from 'services/postService';
+import { getPosts } from '../services/postService';
 import Spinner from 'components/Spinner';
 
 const Home = () => {

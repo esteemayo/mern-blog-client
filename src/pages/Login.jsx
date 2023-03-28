6df84c82025a28d174fc8678bd71131033baad35
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-29 00:38:32 +0100

    fix: fix a bug

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 import Button from '../components/Button';
 import { useGlobalContext } from '../context/auth/AuthContext';
-import { loginUser } from 'services/userService';
+import { loginUser } from '../services/userService';
 import { LOGIN_START } from 'context/auth/AuthTypes';
 
 const Login = () => {

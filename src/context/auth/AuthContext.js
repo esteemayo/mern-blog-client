4f247227a189ead4eeec50e9eb214037052991ec
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-29 00:52:09 +0100

    fix: fix a bug

--- src/context/auth/AuthContext.js
+++ src/context/auth/AuthContext.js
@@ -2,7 +2,7 @@ import jwtDecode from 'jwt-decode';
 import React, { useContext, useReducer, createContext } from 'react';
 
 import Reducer from './AuthReducer';
-import { getJwt } from 'services/userService';
+import { getJwt } from '../services/userService';
 import * as actions from './AuthTypes';
 import {
   getFromStorage,

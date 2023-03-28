e912266c3f8f7f4118eebd249e55e207db0bd582
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-29 00:38:43 +0100

    fix: fix a bug

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import Button from '../components/Button';
 import { useGlobalContext } from '../context/auth/AuthContext';
 import { loginUser } from '../services/userService';
-import { LOGIN_START } from 'context/auth/AuthTypes';
+import { LOGIN_START } from '../context/auth/AuthTypes';
 
 const Login = () => {
   const { isFetching, dispatch, loginSuccess, loginFailure } =

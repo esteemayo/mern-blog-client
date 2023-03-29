76efbd15c8e1b0e932885ef37890162d0adaeffe
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-29 03:16:15 +0100

    fix: modify

--- src/utils/index.js
+++ src/utils/index.js
@@ -7,7 +7,7 @@ export const getFromStorage = (key) => {
 
 export const setToStorage = (key, value) => {
   if (typeof window !== 'undefined')
-  return localStorage.setItem(key, JSON.stringify(value));
+    return localStorage.setItem(key, JSON.stringify(value));
 }
 
 export const removeFromStorage = (key) =>

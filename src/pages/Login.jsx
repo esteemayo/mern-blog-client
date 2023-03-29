8c967dcdf49104220785517fb76b5eb0ff69e8cb
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-29 03:10:53 +0100

    feat: import react-toastify module

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 import Button from '../components/Button';
 import { useGlobalContext } from '../context/auth/AuthContext';

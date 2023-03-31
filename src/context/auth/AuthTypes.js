a06de116428407a72a7bc8d898a0c02257e818c6
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-31 21:23:21 +0100

    fix: modify

--- src/context/auth/AuthTypes.js
+++ src/context/auth/AuthTypes.js
@@ -3,7 +3,6 @@ export const LOGIN_FAILURE = 'LOGIN_FAILURE';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const RESET = 'RESET';
 export const LOGOUT = 'LOGOUT';
-export const LOGIN_START = 'LOGIN_START';
 export const UPDATE_START = 'UPDATE_START';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAILURE = 'LOGIN_FAILURE';

4fc9757092dd4c530e3a8bfbeb3cf54dce2fbccf
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-01 19:48:59 +0100

    feat: import useEffect react hook

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import Button from '../components/Button';

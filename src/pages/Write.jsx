4ed65107b91d5b0255390bee2bba44e4a2933a3d
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-02 15:15:55 +0100

    feat: import useRef react hook

--- src/pages/Write.jsx
+++ src/pages/Write.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { FaPlus } from 'react-icons/fa';
 
 import Button from '../components/Button';

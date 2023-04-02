560a33f3c5dc27043ff85d5fdd426d09b6798bb7
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-02 15:52:56 +0100

    fix: modify

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 
 import logger from './logService';
-import { getFromStorage, tokenKey } from '../utils';
 
 axios.defaults.baseURL = 'http://localhost:8080/api/v1';
 

a802c0846ddb6fb7b68f327d96d7f434b4c05402
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-02 15:00:43 +0100

    fix: refactor code

--- src/pages/Write.jsx
+++ src/pages/Write.jsx
@@ -129,7 +129,7 @@ const Write = () => {
             <textarea
               placeholder='Tell your story...'
               className='writeInput writeText'
-              onChange={(e) => setDescription(e.target.value)}
+              onChange={handleChange}
             ></textarea>
           </div>
           <Button

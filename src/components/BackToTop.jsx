a0b629a67ffca31a3e859e531550f88bbb0dee41
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-04 03:43:51 +0100

    feat: add isVisible state value

--- src/components/BackToTop.jsx
+++ src/components/BackToTop.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { KeyboardArrowUpOutlined } from '@mui/icons-material';
 
 const BackToTop = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
   return (
     <div className='backtotopContainer'>
       <div className='iconWrapper'>

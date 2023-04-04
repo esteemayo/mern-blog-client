8b66cf01c8e656ac8020b4778311db0883f5bff7
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-04 03:47:23 +0100

    fix: modify

--- src/components/BackToTop.jsx
+++ src/components/BackToTop.jsx
@@ -4,6 +4,11 @@ import { KeyboardArrowUpOutlined } from '@mui/icons-material';
 const BackToTop = () => {
   const [isVisible, setIsVisible] = useState(false);
 
+  window.onscroll = () => {
+    setIsVisible(window.pageYOffset > 500 ? true : false);
+    return () => (window.onscroll === null);
+  }
+
   return (
     <div className='backtotopContainer'>
       <div className='iconWrapper'>

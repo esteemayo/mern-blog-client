1d5ea3af01181fa7b00b70b8abde33d2b397406d
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-18 20:41:33 +0100

    fix: modify

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,6 +16,7 @@ const Home = () => {
 
   useEffect(() => {
     const { token, cancel } = axios.CancelToken.source();
+
     (async () => {
       try {
         const { data } = await getPosts(search, token);
